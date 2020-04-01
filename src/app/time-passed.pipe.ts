ca84b9cbb44abd67f51314ddea2097ff957d3a6a
Buttonupd <Dankariuki0101@gmail.com>
2020-04-02 00:06:46 +0300

    Creating a time functionality

--- src/app/time-passed.pipe.ts
+++ src/app/time-passed.pipe.ts
@@ -7,6 +7,12 @@ export class TimePassedPipe implements PipeTransform {
 
   transform(value: unknown, ...args: unknown[]): unknown {
     return null;
+    let today:Date = new Date();
+    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+
   }
+  
+
+  
 
 }
