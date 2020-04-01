eaf9fd67deb729d94dff630ea898d127de944bdd
Buttonupd <Dankariuki0101@gmail.com>
2020-04-02 00:40:51 +0300

    THe mathematics in coding, the logic

--- src/app/time-passed.pipe.ts
+++ src/app/time-passed.pipe.ts
@@ -4,15 +4,31 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'timePassed'
 })
 export class TimePassedPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-    let today:Date = new Date();
+  transform(value: any): any{
+    let today:Date = new Date(); //get current date and time
     let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    var dateDifference = Math.abs(value - todayWithNoTime) //returns value in miliseconds
+    const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
+    var dateDifferenceSeconds = dateDifference*0.001; //converts miliseconds to seconds
+    var dateCounter = dateDifferenceSeconds/secondsInDay;
 
+    if (dateCounter >= 1 && value > todayWithNoTime){
+      return dateCounter;
+    }else{
+      return 0;
+    }
   }
-  
-
-  
-
 }
+
+ 
+
+    // let today:Date = new Date();
+    // let current:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    // var dateSinceSubmit = Math.abs(value - current);
+    // const getTime = current - dateSinceSubmit;
+    // var timePassed = getTime;
+    // if(timePassed >= 1 && value > current){
+    //   return timePassed;
+    // } else{
+    //   return 0;
+    
