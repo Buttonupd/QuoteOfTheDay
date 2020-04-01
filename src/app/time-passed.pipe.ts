251a0ff8f96fd2cc94c392f026ccc8a46367e842
Buttonupd <Dankariuki0101@gmail.com>
2020-04-02 02:08:07 +0300

    Date sice submission requires improvement/s

--- src/app/time-passed.pipe.ts
+++ src/app/time-passed.pipe.ts
@@ -24,8 +24,8 @@ export class TimePassedPipe implements PipeTransform {
     var dateDifference = Math.abs(value - todayWithNoTime) //returns value in miliseconds
     const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
     var dateDifferenceSeconds = dateDifference*0.001; //converts miliseconds to seconds
-    var dateCounter = dateDifferenceSeconds/secondsInDay;
-    return dateCounter;
+    var dateCounter = dateDifferenceSeconds * secondsInDay /dateDifference  ;
+    return todayWithNoTime;
 
 
 
