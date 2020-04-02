ff0bfbef3dbca7a8d226666ed757a4982ab78cfe
Buttonupd <Dankariuki0101@gmail.com>
2020-04-02 22:05:27 +0300

    Revised draft html components

--- src/app/quoted/quoted.component.ts
+++ src/app/quoted/quoted.component.ts
@@ -9,10 +9,10 @@ import { Qoutes } from '../qoutes';
 export class QuotedComponent implements OnInit {
   quotes:Qoutes[] = [
     new Qoutes('Lincoln', 'Susan', 'Do not ever be afraid of what you are not ', new Date(2020,4,14)),
-    new Qoutes('Garvey', 'Soni', 'Stars cannot shine without darkness ', new Date(2020,5,14)),
-    new Qoutes('Abraham','Button', 'The rose is thorny but a flower too', new Date(2020,4,19)),
-    new Qoutes('Marley','Nemesis','The roots of the righteous', new Date(2020,4,17)),
-    new Qoutes('I&I','Batallion','Tomorrow is another day', new Date(2020,4,18)),
+    // new Qoutes('Garvey', 'Soni', 'Stars cannot shine without darkness ', new Date(2020,5,14)),
+    // new Qoutes('Abraham','Button', 'The rose is thorny but a flower too', new Date(2020,4,19)),
+    // new Qoutes('Marley','Nemesis','The roots of the righteous', new Date(2020,4,17)),
+    // new Qoutes('I&I','Batallion','Tomorrow is another day', new Date(2020,4,18)),
 
   ];
   
